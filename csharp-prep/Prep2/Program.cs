add2a75b332ac0873dc0d3458e4b45eb055e7e16
BenjaminAmaro <benjamin.amaro.sbam@gmail.com>
2023-04-20 21:41:22 -0400

    Prep 2 if statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,27 @@ class Program
     static void Main(string[] args)
     {
         //this is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+        string finalGrade = "";
+        if (intGrade >= 90)
+        {
+            finalGrade = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            finalGrade = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            finalGrade = "C";
+        }
+        else
+        {
+            finalGrade = "F";
+        }
+        Console.WriteLine($"your grade is {finalGrade} ");
     }
+
 }
