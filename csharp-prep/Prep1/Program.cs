0945345fbcb1045a18f56af4d434b2fa6de83259
BenjaminAmaro <benjamin.amaro.sbam@gmail.com>
2023-04-20 20:13:10 -0400

    first part print and input in c#.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    Console.WriteLine("Hello Prep1 World!");
+    int x = 10;
+    int y = 5;
+    if (x > y)
+        {
+            Console.WriteLine("what is your First name? ");
+            string fname = Console.ReadLine();
+
+            Console.WriteLine("what is your Lastname? ");
+            string lname = Console.ReadLine();
+            
+            Console.WriteLine($"your name is {lname}, {fname} {lname}. " );
+        }
     }
 }
