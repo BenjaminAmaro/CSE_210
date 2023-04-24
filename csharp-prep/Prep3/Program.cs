8d631817fe6b2d33ca6aa6947ab29b7eeed57163
BenjaminAmaro <benjamin.amaro.sbam@gmail.com>
2023-04-24 14:18:45 -0400

    prep 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        Console.WriteLine(magicNumber);
+
+        int guess = -1;
+
+        while ( magicNumber != guess)
+        { 
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
