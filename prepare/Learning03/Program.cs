1472912a08f3a0db74dd2aec24be3dd9d4162fe4
BenjaminAmaro <benjamin.amaro.sbam@gmail.com>
2023-05-19 10:55:12 -0400

    Program03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+Fraction f1 = new Fraction(); // Don't pass any information to get the default values
+    Console.WriteLine(f1.GetFractionString());
+    Console.WriteLine(f1.GetDecimalValue());
+Fraction f2 = new Fraction(5); // pass the first and last names
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+Fraction f3 = new Fraction(3,4); // pass all three variables
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
